9bcf944daf97f3e8f5c600b4ce7d62a710dc4cef
Jithin T Jacob <130245080+Jithin-T-Jacob-2022@users.noreply.github.com>
2024-04-01 15:04:30 +0000

    Dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello"); // we are using nodejs, not browser
+
+console.log(3
+    + "3"); // never write like this focus on code readability should be high
+
+console.log("Jithin");
+
+let name = "Jithin"
+let age = 23;
+let isLoggedIn = true;
+
+/*
+Primitive Datatypes:
+    number => 2
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => declared but no value assigned
+    symbol => to find uniqueness
+
+Object:
+    typeOf => to know the type
+*/

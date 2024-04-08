6765a86cd3af92a73f9b72b04773ec7ec5ade315
Jithin T Jacob <130245080+Jithin-T-Jacob-2022@users.noreply.github.com>
2024-04-08 15:44:16 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = 33
+let score1 = "33"
+let score2 = "33abc"
+let score3 = null
+let score4 = undefined
+let score5 = true //if value is false then output is 0
+
+console.log(typeof(score));
+
+console.log(typeof score);
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+console.log(typeof score5);
+
+console.log("-------------------------------");
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+
+console.log("-------------------------------");
+
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(typeof valueInNumber2);
+console.log(typeof valueInNumber3);
+console.log(typeof valueInNumber4);
+console.log(typeof valueInNumber5);
+
+console.log(valueInNumber);
+console.log(valueInNumber1);
+console.log(valueInNumber2);
+console.log(valueInNumber3);
+console.log(valueInNumber4);
+console.log(valueInNumber5);

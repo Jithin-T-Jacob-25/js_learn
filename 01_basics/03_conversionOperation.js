9e25439b60853bab3264d4f404ddf9d90944d3c4
Jithin T Jacob <130245080+Jithin-T-Jacob-2022@users.noreply.github.com>
2024-04-09 15:13:25 +0000

    conversion part 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -38,3 +38,33 @@ console.log(valueInNumber2);
 console.log(valueInNumber3);
 console.log(valueInNumber4);
 console.log(valueInNumber5);
+console.log("\n");
+
+
+console.log("*************************Operaions***************************");
+console.log("\n");
+/*
+
+2 + 2
+2 - 2
+2 * 2
+2 ** 3
+2 / 3
+2 % 3
+
+*/
+
+let str1 = "Hello"
+let str2 = "Jithin"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+""); // never do it
